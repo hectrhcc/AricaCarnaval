498b2e4909fd479e77a78037e92a37cc219708cb
hectrhcc <hcontrerascorvacho@gmail.com>
2025-04-21 18:02:38 -0400

    Agregando tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import React from 'react';
 import './App.css'
 
 function App() {
@@ -6,8 +7,8 @@ function App() {
   return (
     <>
     
-      <h1>Arica Carnaval <br/>en Construcción</h1>
-      <img src="/images/logo_polpaico.webp" alt="Página en construcción" />
+      <h1 className="text-4xl font-bold text-yellow-500">Arica Carnaval <br/>en Construcción</h1>
+      <img src="/images/logo_polpaico.webp" alt="Página en construcción" className='mx-auto pt-6 h-80'/>
     </>
   )
 }
